34db8fbdd4ca52fe857b10c17017faebe7654088
Jonathan Silva Viana <jonathansilva@daum.net>
2018-11-27 22:31:57 +0900

    arquivos comitados com nova direção do app II

--- app/src/main/java/kpopstation/com/br/kpopstation/SemRedeActivity.java
+++ app/src/main/java/kpopstation/com/br/kpopstation/SemRedeActivity.java
@@ -2,12 +2,19 @@ package kpopstation.com.br.kpopstation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SemRedeActivity extends AppCompatActivity {
+
+    String semrede;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sem_rede);
+
+        semrede = "Sem conexão neste dispositivo";
+
+        Toast.makeText(this, semrede, Toast.LENGTH_SHORT).show();
     }
 }
