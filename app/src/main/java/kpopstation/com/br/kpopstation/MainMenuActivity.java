d6c023fc7c929c60054ea7f9ec959c8e993fb224
Jonathan Silva Viana <jonathansilva@daum.net>
2018-11-28 00:35:56 +0900

    arquivos comitados com nova direção do app III

--- app/src/main/java/kpopstation/com/br/kpopstation/MainMenuActivity.java
+++ app/src/main/java/kpopstation/com/br/kpopstation/MainMenuActivity.java
@@ -5,10 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     FloatingActionButton bt_email;
+
+    Button posts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,18 @@ public class MainMenuActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent vaiparaemail = new Intent(MainMenuActivity.this, EmailActivity.class);
                 startActivity(vaiparaemail);
+            }
+        });
+
+
+        //botões de posts
+        posts = (Button)findViewById(R.id.bt_posts_view);
+
+        posts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent enviauseraposts = new Intent(MainMenuActivity.this, SemRedeActivity.class);
+                startActivity(enviauseraposts);
             }
         });
     }
--- app/src/main/java/kpopstation/com/br/kpopstation/SemRedeActivity.java
+++ app/src/main/java/kpopstation/com/br/kpopstation/SemRedeActivity.java
@@ -1,11 +1,18 @@
 package kpopstation.com.br.kpopstation;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class SemRedeActivity extends AppCompatActivity {
 
+    Button sair_do_app;
     String semrede;
 
     @Override
@@ -13,8 +20,48 @@ public class SemRedeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sem_rede);
 
+
+        //aviso que está sem rede
         semrede = "Sem conexão neste dispositivo";
 
         Toast.makeText(this, semrede, Toast.LENGTH_SHORT).show();
+
+        //dá ação de sair do aplicativo
+        sair_do_app = (Button)findViewById(R.id.bt_sair);
+
+        sair_do_app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //android.os.Process.killProcess(android.os.Process.myPid());
+                //System.exit(0);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(SemRedeActivity.this);
+                alertDialogBuilder.setTitle("Problemas de rede");
+                alertDialogBuilder
+                        .setMessage("Clique em sim para fechar")
+                        .setCancelable(false)
+                        .setPositiveButton("Sim",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        moveTaskToBack(true);
+                                        android.os.Process.killProcess(android.os.Process.myPid());
+                                        System.exit(1);
+                                    }
+                                })
+
+                        .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                dialog.cancel();
+                                Intent mandaparahomedenovo = new Intent(SemRedeActivity.this, MainActivity.class);
+                                startActivity(mandaparahomedenovo);
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
+
     }
 }
